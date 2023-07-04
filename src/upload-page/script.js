62b7528a92b015c35dbaa34383129015679861a1
shakilmiahcse <shakilmiahr55@gmail.com>
2023-07-04 09:54:10 +0600

    upload modifyed

--- src/upload-page/script.js
+++ src/upload-page/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $.ajax({
-            URL:'categories.php',
+            url:'categories.php',
             type:'GET',
             success: function(response){
                 $('#categoryId').html(response);
