79af675092c3c19d5e10ba490dd1b4aac4c442bc
shakilmiahcse <shakilmiahr55@gmail.com>
2023-07-06 12:09:47 +0600

    book post added

--- registration/sregi/homee.php
+++ registration/sregi/homee.php
@@ -67,7 +67,7 @@ session_start();
                                             // Check if session variable is set for the profile picture
                                             if (isset($_SESSION['profilepic'])) {
                                                 $profilePic = $_SESSION['profilepic'];
-                                                echo '<img src="' . $profilePic . '" style="width: 30px; height: 30px;" class="img-fluid rounded-circle" alt="Profile Picture">';
+                                                echo '<img src="' . $profilePic . '" style="width: 35px; height: 30px; margin: -10px;" class="img-fluid rounded-circle" alt="Profile Picture">';
                                             } else {
                                                 echo '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-bounding-box" viewBox="0 0 16 16">
                                                 <path d="M1.5 1a.5.5 0 0 0-.5.5v3a.5.5 0 0 1-1 0v-3A1.5 1.5 0 0 1 1.5 0h3a.5.5 0 0 1 0 1h-3zM11 .5a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 1 16 1.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 1-.5-.5zM.5 11a.5.5 0 0 1 .5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 1 0 1h-3A1.5 1.5 0 0 1 0 14.5v-3a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v3a1.5 1.5 0 0 1-1.5 1.5h-3a.5.5 0 0 1 0-1h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 1 .5-.5z" />
