73928a4fc63858319de101a13a23fc47762079a8
shakilmiahcse <shakilmiahr55@gmail.com>
2023-07-11 13:06:41 +0600

    login edited

--- login.php
+++ login.php
@@ -10,8 +10,8 @@
 <body>
     <div class="container">
         <div class="row">
-            <div class="col-md-3"></div>
-            <div class="col-md-6">
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
                 <h1 class="text-center mt-5">User Login</h1>
                 <div class="card">
                     <div class="card-header bg-success text-white">Login Now</div>
--- register.php
+++ register.php
@@ -11,8 +11,8 @@
 <body>
     <div class="container">
         <div class="row">
-            <div class="col-md-3"></div>
-            <div class="col-md-6">
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
                 <h1 class="text-center mt-5">User Registration</h1>
                 <div class="card">
                     <div class="card-header bg-success text-white">Registration Now</div>
